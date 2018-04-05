859267f3b9b490c1abebcd72e2fe48e3e73f1f5e
Unknown <mhc6kp@virginia.edu>
2018-04-05 12:51:44 -0400

    upload project

---
+++ hci-fitnote-ios/AddWorkoutViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AddWorkoutViewController.swift
+//  hci-fitnote-ios
+//
+//  Created by Michael Chang on 4/5/18.
+//  Copyright © 2018 Michael Chang. All rights reserved.
+//
+
+import UIKit
+
+class AddWorkoutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancelButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
